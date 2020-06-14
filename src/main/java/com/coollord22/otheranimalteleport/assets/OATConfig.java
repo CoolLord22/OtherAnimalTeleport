6908d42923feb86971c6e5563ac16649845efc9c
CoolLord22 <mastermodmaker@yahoo.com>
2020-06-14 14:21:39 -0400

    Adding worldgroups

--- src/main/java/com/coollord22/otheranimalteleport/assets/OATConfig.java
+++ src/main/java/com/coollord22/otheranimalteleport/assets/OATConfig.java
@@ -8,137 +8,173 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EntityType;
 
 import com.coollord22.otheranimalteleport.OtherAnimalTeleport;
 
 public class OATConfig {
-	private final OtherAnimalTeleport parent;
-	
-	protected static Verbosity verbosity = Verbosity.NORMAL;
-	public static boolean gColorLogMessages;
-	public static int radius;
-	public static List<ArrayList<String>> worldGroup = new ArrayList<ArrayList<String>>();
+	private final OtherAnimalTeleport plugin;
 
-	public OATConfig(OtherAnimalTeleport instance) {
-		parent = instance;
-	}
+	protected static Verbosity 		verbosity = Verbosity.NORMAL;
+	public boolean 					gColorLogMessages;
+	public boolean					globalUpdateChecking;
+	public boolean 					usePrefix = true;
 	
+	public int 						radius;
+	
+	public List<ArrayList<String>> 	worldGroup = new ArrayList<ArrayList<String>>();
+	public List<EntityType> 		allowedEnts = new ArrayList<EntityType>();
+	
+	public String 					prefix;
+	public String 					failedTeleportMessage;
+
+	public OATConfig(OtherAnimalTeleport plugin) {
+		this.plugin = plugin;
+	}
+
 	public void load(CommandSender sender) {
-        List<String> result = new ArrayList<String>();
+		List<String> result = new ArrayList<String>();
 		try { 
-            firstRun();
+			firstRun();
 			loadConfig();
 		} catch (FileNotFoundException e) {
-            if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
-            result.add("Config file not found!");
-            result.add("The error was:\n" + e.toString());
-            result.add("You can fix the error and reload with /orr.");
-            sendMessage(sender, result);
-        } catch (IOException e) {
-            if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
-            result.add("There was an IO error which has forced OtherDrops to abort loading!");
-            result.add("The error was:\n" + e.toString());
-            result.add("You can fix the error and reload with /orr.");
-            sendMessage(sender, result);
-        } catch (InvalidConfigurationException e) {
-            if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
-            result.add("Config is invalid!");
-            result.add("The error was:\n" + e.toString());
-            result.add("You can fix the error and reload with /orr.");
-            sendMessage(sender, result);
-        } catch (NullPointerException e) {
-            result.add("Config load failed!");
-            result.add("The error was:\n" + e.toString());
-            if (verbosity.exceeds(Verbosity.NORMAL)) e.printStackTrace();
-            result.add("Please try the latest version & report this issue to the developer if the problem remains.");
-            sendMessage(sender, result);
-        } catch (Exception e) {
-            if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
-            result.add("Config load failed!  Something went wrong.");
-            result.add("The error was:\n" + e.toString());
-            result.add("If you can fix the error, reload with /orr.");
-            sendMessage(sender, result);
-        }
+			if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
+			result.add("Config file not found!");
+			result.add("The error was:\n" + e.toString());
+			result.add("You can fix the error and reload with /orr.");
+			sendMessage(sender, result);
+		} catch (IOException e) {
+			if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
+			result.add("There was an IO error which has forced OtherDrops to abort loading!");
+			result.add("The error was:\n" + e.toString());
+			result.add("You can fix the error and reload with /orr.");
+			sendMessage(sender, result);
+		} catch (InvalidConfigurationException e) {
+			if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
+			result.add("Config is invalid!");
+			result.add("The error was:\n" + e.toString());
+			result.add("You can fix the error and reload with /orr.");
+			sendMessage(sender, result);
+		} catch (NullPointerException e) {
+			result.add("Config load failed!");
+			result.add("The error was:\n" + e.toString());
+			if (verbosity.exceeds(Verbosity.NORMAL)) e.printStackTrace();
+			result.add("Please try the latest version & report this issue to the developer if the problem remains.");
+			sendMessage(sender, result);
+		} catch (Exception e) {
+			if (verbosity.exceeds(Verbosity.HIGH)) e.printStackTrace();
+			result.add("Config load failed!  Something went wrong.");
+			result.add("The error was:\n" + e.toString());
+			result.add("If you can fix the error, reload with /orr.");
+			sendMessage(sender, result);
+		}
 	}
-	 
+
 	private void sendMessage(CommandSender sender, List<String> result) {
-	        if (sender != null) {
-	            sender.sendMessage(result.toArray(new String[0]));
-	        }
-	        Log.logInfo(result);
-	    }
-	 
-    private void firstRun() throws Exception {
-        List<String> files = new ArrayList<String>();
-        files.add("config.yml");
-        
-        for (String filename : files) {
-            File file = new File(parent.getDataFolder(), filename);
-            if (!file.exists()) {
-                file.getParentFile().mkdirs();
-                copy(parent.getResource(filename), file);
-            }
-        }
-    }
-    
+		if (sender != null) {
+			sender.sendMessage(result.toArray(new String[0]));
+		}
+		plugin.log.logInfo(result);
+	}
+
+	private void firstRun() throws Exception {
+		List<String> files = new ArrayList<String>();
+		files.add("config.yml");
+
+		for (String filename : files) {
+			File file = new File(plugin.getDataFolder(), filename);
+			if (!file.exists()) {
+				file.getParentFile().mkdirs();
+				copy(plugin.getResource(filename), file);
+			}
+		}
+	}
+
 	@SuppressWarnings("unchecked")
 	public void loadConfig() throws FileNotFoundException, IOException, InvalidConfigurationException {
-        String filename = "config.yml";
-        File global = new File(parent.getDataFolder(), filename);
-        YamlConfiguration globalConfig = YamlConfiguration.loadConfiguration(global);
-        // Make sure config file exists (even for reloads - it's possible this
-        // did not create successfully or was deleted before reload)
-        if (!global.exists()) {
-            try {
-                global.createNewFile(); 
-                Log.logInfo("Created a config file " + parent.getDataFolder() + "\\" + filename + ", please edit it!");
-                globalConfig.save(global);
-            } catch (IOException ex) {
-                Log.logWarning(parent.getDescription().getName() + ": could not generate " + filename + ". Are the file permissions OK?");
-            } catch (Exception e) {
-                Log.logWarning(parent.getDescription().getName() + ": could not generate " + filename + ". Are the file permissions OK?");
+		String filename = "config.yml";
+		File global = new File(plugin.getDataFolder(), filename);
+		YamlConfiguration globalConfig = YamlConfiguration.loadConfiguration(global);
+		// Make sure config file exists (even for reloads - it's possible this
+		// did not create successfully or was deleted before reload)
+		if (!global.exists()) {
+			try {
+				global.createNewFile(); 
+				plugin.log.logInfo("Created a config file " + plugin.getDataFolder() + "\\" + filename + ", please edit it!");
+				globalConfig.save(global);
+			} catch (IOException ex) {
+				plugin.log.logWarning(plugin.getDescription().getName() + ": could not generate " + filename + ". Are the file permissions OK?");
+			} catch (Exception e) {
+				plugin.log.logWarning(plugin.getDescription().getName() + ": could not generate " + filename + ". Are the file permissions OK?");
 			}
-        }
-        
-        // Load in the values from the configuration file
-        globalConfig.load(global);
+		}
 
-        worldGroup.clear();
-        verbosity = OATCommon.getConfigVerbosity(globalConfig);
-        gColorLogMessages = globalConfig.getBoolean("color_log_messages", true);
-        radius = globalConfig.getInt("radius", 2);
-        
-        for(Object input : globalConfig.getList("groups")) {
-        	worldGroup.add((ArrayList<String>)input);
-        }
+		// Load in the values from the configuration file
+		globalConfig.load(global);
 
-        Log.logInfo("Loaded global config (" + global + "), keys found: " + " (verbosity=" + verbosity + ")", Verbosity.HIGHEST);
-    }
+		worldGroup.clear();
+		allowedEnts.clear();
 
-    public static Verbosity getVerbosity() {
-        return verbosity;
-    }
+		verbosity = OATCommon.getConfigVerbosity(globalConfig);
+		globalUpdateChecking = globalConfig.getBoolean("update_checker", true);
+		gColorLogMessages = globalConfig.getBoolean("color_log_messages", true);
+		radius = globalConfig.getInt("radius", 2);
+		
+		usePrefix = globalConfig.getBoolean("use_prefix", true);
+		prefix = globalConfig.getString("prefix", "&7[&aOtherAnimalTeleport&7] ");
+		
+		//messages
+		failedTeleportMessage = globalConfig.getString("fail_teleport", "&7An entity could not be teleported and is located near (&c%x&7, &c%y&7, &c%z&7).");
 
-    public static void setVerbosity(Verbosity verbosity) {
-        OATConfig.verbosity = verbosity;
-    }
-    
-    private void copy(InputStream in, File file) {
-        try {
-            OutputStream out = new FileOutputStream(file);
-            byte[] buf = new byte[1024];
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-            out.close();
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		for(Object input : globalConfig.getList("groups")) {
+			worldGroup.add((ArrayList<String>)input);
+		}
+		for(String input : globalConfig.getStringList("allowed_entities")) {
+			if(input.equalsIgnoreCase("ANY") || input.equalsIgnoreCase("ALL")) {
+				for(EntityType entType : EntityType.values()) {
+					allowedEnts.add(entType);
+				}
+			} 
+			else {
+				boolean foundMatch = false;
+				for(EntityType entType : EntityType.values()) {
+					if(input.equalsIgnoreCase(entType.toString())) {
+						foundMatch = true;
+						allowedEnts.add(entType);
+					}
+				}
+				if(!foundMatch) {
+					plugin.log.logWarning("Unrecognized entity type (" + input + ")! Skipping...");
+				}
+			}
+		}
+
+		plugin.log.logInfo("Loaded global config (" + global + ") with (verbosity=" + verbosity + ")", Verbosity.HIGHEST);
+	}
+
+	public static Verbosity getVerbosity() {
+		return verbosity;
+	}
+
+	public static void setVerbosity(Verbosity verbosity) {
+		OATConfig.verbosity = verbosity;
+	}
+
+	private void copy(InputStream in, File file) {
+		try {
+			OutputStream out = new FileOutputStream(file);
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			out.close();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }

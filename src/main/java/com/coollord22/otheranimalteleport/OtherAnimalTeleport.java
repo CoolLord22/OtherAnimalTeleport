18323408e046e73b0c1526e3da16abba743a1cc5
CoolLord22 <mastermodmaker@yahoo.com>
2020-06-14 14:20:48 -0400

    Cancelling static abuse and creating writer methods

--- src/main/java/com/coollord22/otheranimalteleport/OtherAnimalTeleport.java
+++ src/main/java/com/coollord22/otheranimalteleport/OtherAnimalTeleport.java
@@ -1,15 +1,33 @@
 package com.coollord22.otheranimalteleport;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import com.coollord22.otheranimalteleport.assets.Log;
+import com.coollord22.otheranimalteleport.assets.OATCommon;
 import com.coollord22.otheranimalteleport.assets.OATConfig;
+import com.coollord22.otheranimalteleport.assets.Updater;
+import com.coollord22.otheranimalteleport.assets.Verbosity;
 import com.coollord22.otheranimalteleport.listeners.OATListeners;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class OtherAnimalTeleport extends JavaPlugin {
 	public static OtherAnimalTeleport plugin;
-	public static OATConfig config;
+	public OATConfig config;
+	public OATCommon common;
+	public Updater updateChecker;
 	public Log log = null;
 
 	public boolean enabled;
@@ -21,21 +39,17 @@ public class OtherAnimalTeleport extends JavaPlugin {
 	@Override
 	public void onEnable() { 
 		plugin = this;
-		registerListeners();
 		initConfig();
-		initLogger();
+		initCommon();
 		registerCommands();
+		registerListeners();
+		if(plugin.config.globalUpdateChecking) {
+			updateChecker = new Updater(this);
+			updateChecker.checkForUpdate(null);
+		}
+		writeNames(EntityType.class);
+		plugin.log.logInfo(ChatColor.GREEN + "AnimalTeleport has been enabled!", Verbosity.LOW);
 		plugin.enabled = true;
-	}
-
-	private void registerCommands() {
-		this.getCommand("oat").setExecutor(new OtherAnimalCommand(this));
-	}
-
-	private void initLogger() {
-		// Set plugin name & version, this must be at the start of onEnable
-		// Used in log messages throughout
-		this.log = new Log(this);
 	}
 
 	private void initConfig() {
@@ -44,8 +58,48 @@ public class OtherAnimalTeleport extends JavaPlugin {
 		config.load(null);
 	}
 
+	private void initCommon() {
+		// Set plugin name & version, this must be at the start of onEnable
+		// Used in log messages throughout
+		this.log = new Log(this);
+		this.common = new OATCommon(this);
+	}
+
 	private void registerListeners() {
 		PluginManager pm = Bukkit.getServer().getPluginManager();
 		pm.registerEvents(new OATListeners(this), this);
 	}
+
+	private void registerCommands() {
+		this.getCommand("oat").setExecutor(new OtherAnimalCommand(this));
+	}
+	
+    public static void writeNames(Class<? extends Enum<?>> e) {
+        writeNames(e.getSimpleName(), e);
+    }
+
+    public static void writeNames(String filename, Class<? extends Enum<?>> e) {
+        List<String> list = new ArrayList<String>();
+
+        for (Enum<?> stuff : e.getEnumConstants()) {
+            if(stuff instanceof Animals)
+                list.add(stuff.toString());
+        }
+
+        try {
+            BufferedWriter out = null;
+            File folder = plugin.getDataFolder();
+            File configFile = new File(folder.getAbsolutePath() + File.separator + "known_" + filename + ".txt");
+            configFile.getParentFile().mkdirs();
+            configFile.createNewFile();
+            out = new BufferedWriter(new FileWriter(configFile));
+            Collections.sort(list);
+            for(String mat : list)
+                out.write(mat + "\n");
+            out.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
 }

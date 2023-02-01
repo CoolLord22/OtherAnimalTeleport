1553160bca870b251902801ada260efc5b888750
CoolLord22 <mastermodmaker@yahoo.com>
2023-02-01 16:27:55 -0500

    Cleaning up main listener
    Listener now adds checks for unknown/dismount teleports, has world group checking using plugin common methods, etc.

--- src/main/java/com/coollord22/otheranimalteleport/listeners/OATListeners.java
+++ src/main/java/com/coollord22/otheranimalteleport/listeners/OATListeners.java
@@ -24,7 +24,6 @@ import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 public class OATListeners implements Listener {
 
 	private final OtherAnimalTeleport plugin;
-	private final DecimalFormat df = new DecimalFormat("#.#");
 
 	public OATListeners(OtherAnimalTeleport plugin) {
 		this.plugin = plugin;
@@ -39,66 +38,64 @@ public class OATListeners implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onTeleport(PlayerTeleportEvent event) {
-		boolean sameGroup = false;
-		World fromWorld = event.getFrom().getWorld();
-		World toWorld = event.getTo().getWorld();
-		if(plugin.config.blockedRegions.containsKey(toWorld)) {
-			for(ProtectedRegion region : plugin.config.blockedRegions.get(toWorld)) {
-				if(region.contains(BlockVector3.at(event.getTo().getX(), event.getTo().getY(), event.getTo().getZ()))) {
-					plugin.log.logInfo("Player teleporting into a blocked region; ignoring entity checks.", Verbosity.HIGHEST);
+		if(plugin.enabled && !event.isCancelled()) {
+			if(plugin.config.ignoreUnknownCauses && event.getCause() == PlayerTeleportEvent.TeleportCause.UNKNOWN) {
+				plugin.log.logInfo("Ignore unknown cause was enabled, ignoring this event.", Verbosity.HIGHEST);
+				return;
+			}
+
+			/*TODO: Wait for this to be added into 1.19.3*/
+			try {
+				if(event.getCause() == PlayerTeleportEvent.TeleportCause.DISMOUNT) {
+					plugin.log.logInfo("Player dismounted vehicle, ignoring teleport event.", Verbosity.HIGHEST);
 					return;
 				}
+			} catch(NoSuchFieldError err) {
+				plugin.log.logInfo("Version < 1.19.3 so no DISMOUNT cause found.", Verbosity.HIGHEST);
 			}
-		}
-		if(fromWorld.equals(toWorld)) {
-			sameGroup = true;
-		} else {
-			for(Set<World> worldList : plugin.config.worldGroup) {
-				if(worldList.contains(fromWorld) && worldList.contains(toWorld)) {
-					sameGroup = true;
-					break;
-				}
-			}
-		}
-		if(!sameGroup)
-			plugin.log.logInfo("From and To worlds were not found in same group, ending checks.", Verbosity.HIGH);
-		if(plugin.enabled && !event.isCancelled() && sameGroup) {
-			plugin.log.logInfo("From and To worlds were in same group, allowing permission check.", Verbosity.HIGHEST);
+
 			if(event.getPlayer().hasPermission("otheranimalteleport.player.use")) {
-				plugin.log.logInfo("Player permission check passed, gathering nearby entities.", Verbosity.HIGHEST);
-				int radius = plugin.config.radius;
-				boolean toSendError = false;
-				boolean toSendLeft = false;
-				for(Entity ent : event.getFrom().getWorld().getNearbyEntities(event.getFrom(), radius, radius, radius)) {
-					plugin.log.logInfo("Found an entity to teleport: " + ent.getType() + " . Checking if it is allowed.", Verbosity.HIGHEST);
-					if(plugin.config.allowedEnts.contains(ent.getType())) {
-						plugin.log.logInfo("Entity check passed, seeing if player has leashed permissions.", Verbosity.HIGHEST);
-						if(ent instanceof LivingEntity && event.getPlayer().hasPermission("otheranimalteleport.player.teleportleashed")) {
-							if(((LivingEntity) ent).isLeashed() && ((LivingEntity) ent).getLeashHolder().equals(event.getPlayer())) {
-								try {
-									plugin.log.logInfo("Attempting to send leashed entity: " + ent.getType() + ".", Verbosity.HIGHEST);
-									OATMethods.teleportLeashedEnt(ent, event.getFrom(), event.getTo(), event.getPlayer(), plugin);
-									continue;
-								} catch(Exception e) {
-									toSendError = true;
-									continue;
-								}
-							}
-							toSendLeft  = true;
-						}
-						if(ent instanceof Tameable && event.getPlayer().hasPermission("otheranimalteleport.player.teleportpets")) {
-							if(((Tameable) ent).isTamed() && ((Tameable) ent).getOwner() != null && ((Tameable) ent).getOwner().equals(event.getPlayer())) {
-								if(ent instanceof Sittable && !((Sittable) ent).isSitting()) {
+				plugin.log.logInfo("Player use permission check passed, running world checks.", Verbosity.HIGHEST);
+
+				if(plugin.common.checkWorldGroup(event)) {
+					plugin.log.logInfo("World group check passed, gathering nearby entities.", Verbosity.HIGHEST);
+					int radius = plugin.config.radius;
+					boolean toSendError = false;
+					boolean toSendLeft = false;
+
+					for(Entity ent : event.getFrom().getWorld().getNearbyEntities(event.getFrom(), radius, radius, radius)) {
+						plugin.log.logInfo("Found an entity to teleport: " + ent.getType() + " . Checking if it is allowed.", Verbosity.HIGHEST);
+						if(plugin.config.allowedEnts.contains(ent.getType())) {
+							plugin.log.logInfo("Entity check passed, seeing if player has leashed permissions.", Verbosity.HIGHEST);
+							if(ent instanceof LivingEntity && event.getPlayer().hasPermission("otheranimalteleport.player.teleportleashed")) {
+								if(((LivingEntity) ent).isLeashed() && ((LivingEntity) ent).getLeashHolder().equals(event.getPlayer())) {
 									try {
-										plugin.log.logInfo("Attempting to send pet entity: " + ent.getType() + ".", Verbosity.HIGHEST);
-										OATMethods.teleportEnt(ent, event.getFrom(), event.getTo(), event.getPlayer(), plugin);
+										plugin.log.logInfo("Attempting to send leashed entity: " + ent.getType() + ".", Verbosity.HIGHEST);
+										OATMethods.teleportLeashedEnt(ent, event.getFrom(), event.getTo(), event.getPlayer(), plugin);
 										continue;
 									} catch(Exception e) {
 										toSendError = true;
 										continue;
 									}
 								}
+								toSendLeft  = true;
 							}
+							if(ent instanceof Tameable && event.getPlayer().hasPermission("otheranimalteleport.player.teleportpets")) {
+								if(((Tameable) ent).isTamed() && ((Tameable) ent).getOwner() != null && ((Tameable) ent).getOwner().equals(event.getPlayer())) {
+									if(ent instanceof Sittable && !((Sittable) ent).isSitting()) {
+										try {
+											plugin.log.logInfo("Attempting to send pet entity: " + ent.getType() + ".", Verbosity.HIGHEST);
+											OATMethods.teleportEnt(ent, event.getFrom(), event.getTo(), event.getPlayer(), plugin);
+											continue;
+										} catch(Exception e) {
+											toSendError = true;
+											continue;
+										}
+									}
+								}
+								toSendLeft  = true;
+							}
+						}
 					}
 
 					if(plugin.config.failedTeleportMessage != null && !plugin.config.failedTeleportMessage.isEmpty() && toSendError) {
@@ -110,5 +107,7 @@ public class OATListeners implements Listener {
 					}
 				}
 			}
+		}
+		plugin.log.logInfo("Event was cancelled/plugin was disabled, ignoring teleport.", Verbosity.HIGHEST);
 	}
 }

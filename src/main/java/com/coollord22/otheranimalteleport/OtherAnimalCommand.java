99202d0083f32f01a5b8a0f3dce9069d765bc67a
CoolLord22 <mastermodmaker@yahoo.com>
2023-10-30 02:08:02 -0400

    Redoing OAT command handler
    Previous versions had an issue where not using /oat would result in the command not executing. Refactored how the entire command handler works, to now treat all command aliases equally.

--- src/main/java/com/coollord22/otheranimalteleport/OtherAnimalCommand.java
+++ src/main/java/com/coollord22/otheranimalteleport/OtherAnimalCommand.java
@@ -1,19 +1,16 @@
 package com.coollord22.otheranimalteleport;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.Permissible;
 import org.bukkit.util.StringUtil;
 
-import com.coollord22.otheranimalteleport.assets.Verbosity;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OtherAnimalCommand implements TabExecutor {
 	private final OtherAnimalTeleport plugin;
@@ -24,42 +21,28 @@ public class OtherAnimalCommand implements TabExecutor {
 		plugin.getCommand("oat").setTabCompleter(this);
 	}
 
-	private enum OATCommand {
-		HELP("help", "otheranimalteleport.player.help"),
-		RELOAD("reload", "otheranimalteleport.admin.reloadconfig");
-
-		private final String cmdName;
-		private final String perm;
-
-		OATCommand(String name, String perm) {
-			cmdName = name;
-			this.perm = perm;
-		}
-
-		public static OATCommand match(String label, String firstArg) {
-			boolean arg = label.equalsIgnoreCase("oat");
-			for (OATCommand cmd : values()) {
-				if (arg) {
-					for (String item : cmd.cmdName.split(",")) {
-						if (firstArg.equalsIgnoreCase(item))
-							return cmd;
-					}
-				}
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(command.getName().equalsIgnoreCase("oat") && args.length > 0) {
+			if(args[0].equalsIgnoreCase("reload") && sender.hasPermission("otheranimalteleport.admin.reloadconfig")) {
+				cmdReload(sender);
+				return true;
+			} else if(args[0].equalsIgnoreCase("help") && sender.hasPermission("otheranimalteleport.player.help")) {
+				cmdHelp(sender);
+				return true;
 			}
-			return null;
-		}
+        }
+        return false;
+    }
 
-		public String[] trim(String[] args, StringBuffer name) {
-			if (args.length == 0)
-				return args;
-			if (!args[0].equalsIgnoreCase(cmdName))
-				return args;
-			String[] newArgs = new String[args.length - 1];
-			System.arraycopy(args, 1, newArgs, 0, newArgs.length);
-			if (name != null)
-				name.append(" ").append(args[0]);
-			return newArgs;
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		if(command.getName().equalsIgnoreCase("oat")) {
+			if(args.length == 1) {
+				return StringUtil.copyPartialMatches(args[0], Arrays.asList("reload", "help"), new ArrayList<>());
+			}
 		}
+		return null;
 	}
 
 	private String getName(CommandSender sender) {
@@ -70,54 +53,6 @@ public class OtherAnimalCommand implements TabExecutor {
 		else
 			return "UNKNOWN";
 	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		OATCommand cmd = OATCommand.match(label, args.length >= 1 ? args[0] : "");
-		if (cmd == null)
-			return false;
-		StringBuffer cmdName = new StringBuffer(label);
-		args = cmd.trim(args, cmdName);
-
-		if (!checkCommandPermissions(sender, args, cmd))
-			return true;
-
-		switch (cmd) {
-		case RELOAD:
-			cmdReload(sender);
-			break;
-		case HELP:
-			cmdHelp(sender);
-			break;
-		default:
-			break;
-		}
-		return true;
-	}
-
-	private boolean checkCommandPermissions(CommandSender sender, String[] args, OATCommand cmd) {
-		boolean pass = false;
-		if (cmd.perm.isEmpty())
-			pass = true;
-		else if (hasPermission(sender, cmd.perm))
-			pass = true;
-
-		if (!pass)
-			plugin.common.sendMessage(true, sender, "You don't have permission for this command.");
-		return pass;
-	}
-
-	public boolean hasPermission(Permissible who, String permission) {
-        if (who instanceof ConsoleCommandSender)
-            return true;
-        boolean perm = who.hasPermission(permission);
-        if (!perm) {
-        	plugin.log.logInfo("SuperPerms - permission (" + permission + ") denied for " + who, Verbosity.HIGHEST);
-        } else {
-        	plugin.log.logInfo("SuperPerms - permission (" + permission + ") allowed for " + who, Verbosity.HIGHEST);
-        }
-        return perm;
-    }
 
 	private void cmdHelp(CommandSender sender) {
 		List<String> result = new ArrayList<>();
@@ -134,15 +69,5 @@ public class OtherAnimalCommand implements TabExecutor {
 		plugin.config.load(sender);
 		plugin.common.sendMessage(true, sender, "&aOtherAnimalTeleport config reloaded.");
 		plugin.log.logInfo("Config reloaded by " + getName(sender) + ".");
-	}
-
-	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		if(command.getName().equalsIgnoreCase("oat")) {
-			if(args.length == 1) {
-				return StringUtil.copyPartialMatches(args[0], Arrays.asList("reload", "help"), new ArrayList<>());
-			}
-		}
-		return null;
 	}
 }
